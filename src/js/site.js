df026406bcc84d59e9eb5ee93661355d8ed8c1e4
Tobias Lindberg <uet.lindberg@outlook.com>
2024-02-09 09:50:50 +0100

    -

---
+++ src/js/site.js
@@ -0,0 +1,10 @@
+class Article {
+  constructor(title, price, info, imgUrl) {
+    this.title = title;
+    this.price = price;
+    this.info = info;
+    this.imgUrl = imgUrl;
+  }
+}
+
+const articles = [];
